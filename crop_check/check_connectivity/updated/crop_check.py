c7f780a13290955071396a32b2e690474cbca899
David Ackerman <ackermand@janelia.hhmi.org>
2024-12-13 17:13:07 -0500

    updated for s3

--- crop_check/check_connectivity/updated/crop_check.py
+++ crop_check/check_connectivity/updated/crop_check.py
@@ -74,265 +74,6 @@ for current_collection in collections:
 
             with open(f"{crop_dir}/run-config.yaml", "w") as f:
                 yaml.dump(run_config_yaml, f)
-# # for dataset in *; do cd $dataset; for crop in *; do cd $crop; for organelle in *; do bsub -n 4 -P cellmap -o /dev/null -e /dev/null meshify -n 4 $organelle; done; cd ..; done; sleep 90s; cd ..; done;
-# # %%
-
-# import pandas as pd
-# import os
-# import yaml
-# import shutil
-
-# organelles_that_failed = [
-#     "jrc_cos7-1a/crop243/all",
-#     "jrc_cos7-1a/crop243/ecs",
-#     "jrc_cos7-1a/crop292/cell",
-#     "jrc_fly-mb-1a/crop123/cell",
-#     "jrc_fly-mb-1a/crop134/cell",
-#     "jrc_hela-2/crop54/all",
-#     "jrc_hela-2/crop54/ecs",
-#     "jrc_hela-2/crop55/all",
-#     "jrc_hela-2/crop55/ecs",
-#     "jrc_hela-2/crop56/all",
-#     "jrc_hela-2/crop56/ecs",
-#     "jrc_hela-2/crop57/all",
-#     "jrc_hela-2/crop57/ecs",
-#     "jrc_hela-2/crop58/all",
-#     "jrc_hela-2/crop58/ecs",
-#     "jrc_hela-2/crop59/all",
-#     "jrc_hela-2/crop59/ecs",
-#     "jrc_hela-2/crop94/all",
-#     "jrc_hela-2/crop94/nuc",
-#     "jrc_hela-2/crop95/all",
-#     "jrc_hela-2/crop95/nuc",
-#     "jrc_hela-2/crop96/all",
-#     "jrc_hela-2/crop96/nuc",
-#     "jrc_hela-2/crop113/all",
-#     "jrc_hela-2/crop113/cyto",
-#     "jrc_hela-2/crop113/er_mem",
-#     "jrc_hela-2/crop113/er_mem_all",
-#     "jrc_hela-2/crop155/all",
-#     "jrc_hela-2/crop155/chrom",
-#     "jrc_hela-2/crop155/er_mem_all",
-#     "jrc_hela-2/crop155/hchrom",
-#     "jrc_hela-2/crop155/nucpl",
-#     "jrc_hela-3/crop60/all",
-#     "jrc_hela-3/crop60/ecs",
-#     "jrc_hela-3/crop61/all",
-#     "jrc_hela-3/crop61/ecs",
-#     "jrc_hela-3/crop62/all",
-#     "jrc_hela-3/crop62/ecs",
-#     "jrc_hela-3/crop63/all",
-#     "jrc_hela-3/crop63/ecs",
-#     "jrc_hela-3/crop64/all",
-#     "jrc_hela-3/crop64/ecs",
-#     "jrc_hela-3/crop65/all",
-#     "jrc_hela-3/crop65/ecs",
-#     "jrc_hela-3/crop85/all",
-#     "jrc_hela-3/crop85/nuc",
-#     "jrc_hela-3/crop86/all",
-#     "jrc_hela-3/crop86/nuc",
-#     "jrc_hela-3/crop87/all",
-#     "jrc_hela-3/crop87/nuc",
-#     "jrc_hela-3/crop111/all",
-#     "jrc_hela-3/crop111/cyto",
-#     "jrc_hela-3/crop111/er_mem",
-#     "jrc_hela-3/crop111/er_mem_all",
-#     "jrc_jurkat-1/crop66/all",
-#     "jrc_jurkat-1/crop66/ecs",
-#     "jrc_jurkat-1/crop67/all",
-#     "jrc_jurkat-1/crop67/ecs",
-#     "jrc_jurkat-1/crop68/all",
-#     "jrc_jurkat-1/crop68/ecs",
-#     "jrc_jurkat-1/crop69/all",
-#     "jrc_jurkat-1/crop69/ecs",
-#     "jrc_jurkat-1/crop70/all",
-#     "jrc_jurkat-1/crop70/ecs",
-#     "jrc_jurkat-1/crop71/all",
-#     "jrc_jurkat-1/crop71/ecs",
-#     "jrc_jurkat-1/crop91/all",
-#     "jrc_jurkat-1/crop91/nuc",
-#     "jrc_jurkat-1/crop92/all",
-#     "jrc_jurkat-1/crop92/nuc",
-#     "jrc_jurkat-1/crop93/all",
-#     "jrc_jurkat-1/crop93/nuc",
-#     "jrc_macrophage-2/crop72/all",
-#     "jrc_macrophage-2/crop72/ecs",
-#     "jrc_macrophage-2/crop73/all",
-#     "jrc_macrophage-2/crop73/ecs",
-#     "jrc_macrophage-2/crop74/all",
-#     "jrc_macrophage-2/crop74/ecs",
-#     "jrc_macrophage-2/crop75/all",
-#     "jrc_macrophage-2/crop75/ecs",
-#     "jrc_macrophage-2/crop76/all",
-#     "jrc_macrophage-2/crop76/ecs",
-#     "jrc_macrophage-2/crop77/all",
-#     "jrc_macrophage-2/crop77/ecs",
-#     "jrc_macrophage-2/crop88/all",
-#     "jrc_macrophage-2/crop88/nuc",
-#     "jrc_macrophage-2/crop89/all",
-#     "jrc_macrophage-2/crop89/nuc",
-#     "jrc_macrophage-2/crop90/all",
-#     "jrc_macrophage-2/crop90/nuc",
-#     "jrc_macrophage-2/crop110/all",
-#     "jrc_macrophage-2/crop110/cyto",
-#     "jrc_macrophage-2/crop110/er_mem",
-#     "jrc_macrophage-2/crop110/er_mem_all",
-#     "jrc_mus-kidney/crop156/er_mem",
-#     "jrc_mus-kidney/crop158/cell",
-#     "jrc_mus-kidney/crop159/cell",
-#     "jrc_mus-liver/crop132/cell",
-#     "jrc_mus-liver/crop135/cell",
-#     "jrc_mus-liver/crop136/cell",
-#     "jrc_mus-liver/crop137/cell",
-#     "jrc_mus-liver/crop138/cell",
-#     "jrc_mus-liver/crop142/cell",
-#     "jrc_mus-liver/crop143/cell",
-#     "jrc_mus-liver/crop145/all",
-#     "jrc_mus-liver/crop145/cyto",
-#     "jrc_mus-liver/crop145/er_mem",
-#     "jrc_mus-liver/crop145/er_mem_all",
-#     "jrc_mus-liver/crop150/cell",
-#     "jrc_mus-liver/crop151/cell",
-#     "jrc_mus-liver-zon-1/crop266/cell",
-#     "jrc_mus-liver-zon-1/crop268/cell",
-#     "jrc_mus-liver-zon-1/crop269/cell",
-#     "jrc_mus-liver-zon-1/crop270/cell",
-#     "jrc_mus-liver-zon-1/crop272/cell",
-#     "jrc_mus-liver-zon-1/crop273/cell",
-#     "jrc_mus-liver-zon-1/crop274/cell",
-#     "jrc_mus-liver-zon-1/crop275/cell",
-#     "jrc_mus-liver-zon-1/crop276/cell",
-#     "jrc_mus-liver-zon-1/crop280/all",
-#     "jrc_mus-liver-zon-1/crop280/cell",
-#     "jrc_mus-liver-zon-1/crop280/er_mem",
-#     "jrc_mus-liver-zon-1/crop280/er_mem_all",
-#     "jrc_mus-liver-zon-1/crop313/cell",
-#     "jrc_mus-liver-zon-1/crop322/all",
-#     "jrc_mus-liver-zon-1/crop322/cell",
-#     "jrc_mus-liver-zon-1/crop322/er_mem",
-#     "jrc_mus-liver-zon-1/crop322/er_mem_all",
-#     "jrc_mus-liver-zon-1/crop323/cell",
-#     "jrc_mus-liver-zon-1/crop325/cell",
-#     "jrc_mus-liver-zon-1/crop325/mito_mem",
-#     "jrc_mus-liver-zon-1/crop326/cell",
-#     "jrc_mus-liver-zon-1/crop377/cell",
-#     "jrc_mus-liver-zon-2/crop340/cell",
-#     "jrc_mus-liver-zon-2/crop368/cell",
-#     "jrc_mus-liver-zon-2/crop369/cell",
-#     "jrc_mus-liver-zon-2/crop370/cell",
-#     "jrc_mus-liver-zon-2/crop376/cell",
-#     "jrc_mus-nacc-1/crop115/all",
-#     "jrc_mus-nacc-1/crop115/cyto",
-#     "jrc_sum159-1/crop25/all",
-#     "jrc_sum159-1/crop25/ecs",
-#     "jrc_sum159-1/crop26/all",
-#     "jrc_sum159-1/crop26/ecs",
-#     "jrc_sum159-1/crop81/all",
-#     "jrc_sum159-1/crop81/ecs",
-#     "jrc_sum159-1/crop82/all",
-#     "jrc_sum159-1/crop82/ecs",
-#     "jrc_sum159-1/crop83/all",
-#     "jrc_sum159-1/crop83/ecs",
-#     "jrc_sum159-1/crop84/all",
-#     "jrc_sum159-1/crop84/ecs",
-#     "jrc_sum159-1/crop97/all",
-#     "jrc_sum159-1/crop97/nuc",
-#     "jrc_sum159-1/crop98/all",
-#     "jrc_sum159-1/crop98/nuc",
-#     "jrc_sum159-1/crop99/all",
-#     "jrc_sum159-1/crop99/nuc",
-#     "jrc_ut21-1413-003/crop192/all",
-#     "jrc_ut21-1413-003/crop192/pm",
-#     "jrc_ut21-1413-003/crop198/cell",
-#     "jrc_ut21-1413-003/crop199/cell",
-# ]
-# for organelle_that_failed in organelles_that_failed:
-#     print(organelle_that_failed)
-#     current_collection, crop, dataset_name = organelle_that_failed.split("/")
-#     zarr_path = f"/nrs/cellmap/zubovy/crop_splits/combined/{current_collection}/groundtruth.zarr"  # f"/nrs/cellmap/bennettd/data/crop_tests/{current_collection}.zarr"
-#     crop_dir = f"/groups/cellmap/cellmap/ackermand/new_meshes/scripts/single_resolution/crop_check_updated_20241019/{current_collection}/{crop}/{dataset_name}"
-#     os.makedirs(name=crop_dir, exist_ok=True)
-#     os.system(f"cp ../../default_meshes/dask-config.yaml {crop_dir}")
-#     if os.path.exists(
-#         f"/nrs/cellmap/ackermand/new_meshes/meshes/crop_check_updated_20241019/{current_collection}/{crop}/{dataset_name}"
-#     ):
-#         shutil.rmtree(
-#             f"/nrs/cellmap/ackermand/new_meshes/meshes/crop_check_updated_20241019/{current_collection}/{crop}/{dataset_name}"
-#         )
-#     run_config_yaml = {
-#         # "input_path": f"/nrs/cellmap/bennettd/data/crop_tests/{current_collection}.zarr/{crop}/{dataset_name}/s0",
-#         "input_path": f"{zarr_path}/{crop}/{dataset_name}/s0",
-#         "output_directory": f"/nrs/cellmap/ackermand/new_meshes/meshes/crop_check_updated_20241019/{current_collection}/{crop}/{dataset_name}",
-#         "read_write_roi": ["0:512", "0:512", "0:512"],
-#         "downsample_factor": 0,
-#         "target_reduction": 0.5,
-#         "n_smoothing_iter": 0,
-#         "remove_smallest_components": False,
-#         "do_analysis": False,
-#         "do_legacy_neuroglancer": True,
-#     }
-
-#     with open(f"{crop_dir}/run-config.yaml", "w") as f:
-#         yaml.dump(run_config_yaml, f)
-
-# # %%
-# # print out command:
-# for organelle_that_failed in organelles_that_failed:
-#     current_collection, crop, dataset_name = organelle_that_failed.split("/")
-#     print(
-#         f"cd /groups/cellmap/cellmap/ackermand/new_meshes/scripts/single_resolution/crop_check_updated_20241019/{current_collection}/{crop}; bsub -n 12 -P cellmap -o /dev/null -e /dev/null meshify -n 12 {dataset_name}; "
-#     )
-
-# import pandas as pd
-# import neuroglancer
-# from funlib.persistence import open_ds
-# import os
-
-
-# def add_local_volumes(collection, crop, state):
-#     zarr_path = f"/nrs/cellmap/bennettd/data/crop_tests/{collection}.zarr"
-#     print(f"{zarr_path}/{crop}")
-#     for dataset_name in os.listdir(f"{zarr_path}/{crop}"):
-#         if not os.path.isdir(f"{zarr_path}/{crop}/{dataset_name}"):
-#             continue
-#         ds = open_ds(zarr_path, crop + f"/{dataset_name}/s0")
-#         data = ds.to_ndarray()
-#         local_volume = neuroglancer.LocalVolume(
-#             data=data,
-#             dimensions=neuroglancer.CoordinateSpace(
-#                 names=["z", "y", "x"],
-#                 units=["nm", "nm", "nm"],
-#                 scales=ds.voxel_size,
-#                 coordinate_arrays=[
-#                     None,
-#                     None,
-#                     None,
-#                 ],
-#             ),
-#             voxel_offset=ds.roi.begin,
-#         )
-
-#         state.layers.append(name=f"{crop}_{dataset_name}", layer=local_volume)
-
-
-# df = pd.read_csv(
-#     "/groups/scicompsoft/home/ackermand/Programming/segmentation_challenge_meshes/crop_check/Segmentation Challenge _ List of Collections and Crops  - Sheet1.csv"
-# )
-
-# collections = df.collection.unique()
-# neuroglancer.set_server_bind_address("0.0.0.0")
-# viewer = neuroglancer.Viewer()
-
-# current_collection = collections[0]
-# with viewer.txn() as s:
-#     s.layers["raw"] = neuroglancer.ImageLayer(
-#         source="precomputed://gs://neuroglancer-public-data/flyem_fib-25/image",
-#     )
-#     for _, row in df.iterrows():
-#         if row.collection == current_collection:
-#             crop = f"crop{row.crop_number}"
-#             add_local_volumes(row.collection, crop, s)
-# print(viewer)
 
 # %%
 # ## Write out neuroglancer jsons
@@ -352,9 +93,9 @@ def is_dir_empty_except_hidden(path):
 
 neuroglancer.set_server_bind_address("0.0.0.0")
 
-
+df_contrasts = pd.read_csv("SegmentationChallengeContrast.csv")
 df = pd.read_csv(
-    "/groups/scicompsoft/home/ackermand/Programming/segmentation_challenge_meshes/crop_check/check_connectivity/updated/Segmentation Challenge _ List of Collections and Crops  - Sheet1.csv"
+    "/groups/scicompsoft/home/ackermand/Programming/segmentation-challenge-meshes/crop_check/check_connectivity/updated/Segmentation Challenge _ List of Collections and Crops  - Sheet1.csv"
 )
 df = df[df.collection != "jrc_mus-pancreas-1"]
 df.sort_values(by=["collection", "crop_number"], inplace=True)
@@ -363,14 +104,16 @@ collections = df.collection.unique()
 assert len(collections) == 22
 all_info = []
 failed_meshification = []
-only_do_alls = True
+only_do_alls = False
 for current_collection in collections:
-    if only_do_alls:
-        output_dir = (
-            f"/nrs/cellmap/ackermand/crop_check_updated_{date}/viewer_states_all/"
-        )
-    else:
-        output_dir = f"/nrs/cellmap/ackermand/crop_check_updated_{date}/viewer_states/{current_collection}"
+    collection_contrast = df_contrasts.loc[
+        df_contrasts["collection"] == current_collection
+    ]
+    contrast_min = int(collection_contrast["min"].iloc[0])
+    contrast_max = int(collection_contrast["max"].iloc[0])
+
+    output_dir = f"/nrs/cellmap/ackermand/viewer_states/crop_check_updated_{date}/janelia-cosem-datasets/{current_collection}"
+
     os.makedirs(output_dir, exist_ok=True)
 
     collection_df = df[df["collection"] == current_collection]
@@ -381,6 +124,7 @@ for current_collection in collections:
     with viewer.txn() as s:
         s.layers["raw"] = neuroglancer.ImageLayer(
             source=f"zarr://s3://janelia-cosem-datasets/{current_collection}/{current_collection}.zarr/recon-1/em/fibsem-uint8",
+            shaderControls={"normalized": {"range": [contrast_min, contrast_max]}},
         )
     have_set_position = False
     for _, row in collection_df.iterrows():
@@ -404,6 +148,9 @@ for current_collection in collections:
             with viewer.txn() as s:
                 s.layers["raw"] = neuroglancer.ImageLayer(
                     source=f"zarr://s3://janelia-cosem-datasets/{current_collection}/{current_collection}.zarr/recon-1/em/fibsem-uint8",
+                    shaderControls={
+                        "normalized": {"range": [contrast_min, contrast_max]}
+                    },
                 )
             dataset_names = os.listdir(crop_path)
 
@@ -441,19 +188,23 @@ for current_collection in collections:
                     )
                     if ":0" in mesh_id
                 ]
+                # sort segments by integer value
+                segments = sorted(segments, key=lambda x: int(x))
             except Exception as e:
                 print(e, f"{current_collection}/{crop}/{dataset_name}")
                 failed_meshification.append(
                     f"{current_collection}/{crop}/{dataset_name}"
                 )
                 pass
-            image_source = f'zarr://https://cellmap-vm1.int.janelia.org/{crop_path.replace("/nrs/cellmap/","/nrs/")}/{dataset_name}'
+            # image_source = f'zarr://https://cellmap-vm1.int.janelia.org/{crop_path.replace("/nrs/cellmap/","/nrs/")}/{dataset_name}'
+            image_source = f"zarr://s3://janelia-cosem-datasets/{current_collection}/{current_collection}.zarr/recon-1/labels/groundtruth/{crop}/{dataset_name}"
             if solid_chunk:
                 s.layers[f"{crop}_{dataset_name}"] = neuroglancer.SegmentationLayer(
                     source=[image_source]
                 )
             else:
-                mesh_source = f"precomputed://https://cellmap-vm1.int.janelia.org/nrs/ackermand/new_meshes/meshes/crop_check_updated_{date}/{current_collection}/{crop}/{dataset_name}/meshes"
+                # mesh_source = f"precomputed://https://cellmap-vm1.int.janelia.org/nrs/ackermand/new_meshes/meshes/crop_check_updated_{date}/{current_collection}/{crop}/{dataset_name}/meshes"
+                mesh_source = f"precomputed://s3://janelia-cosem-datasets/{current_collection}/neuroglancer/mesh/groundtruth/{crop}/{dataset_name}"
                 s.layers[f"{crop}_{dataset_name}"] = neuroglancer.SegmentationLayer(
                     source=[image_source, mesh_source], segments=segments
                 )
@@ -474,6 +225,9 @@ for current_collection in collections:
                 if only_do_alls:
                     max_xy = np.max(raw_roi_shape[1:])
                     s.projectionScale = 2.5 * max_xy
+                else:
+                    max_xy = np.max(ds.roi.shape[1:])
+                    s.projectionScale = 2 * max_xy
 
                 s.dimensions = neuroglancer.CoordinateSpace(
                     names=["x", "y", "z"],
@@ -488,10 +242,18 @@ for current_collection in collections:
                 s.showSlices = False
                 have_set_position = True
 
-    # Assuming viewer.state.to_json() returns a dictionary
-    viewer_state = s.to_json()
+        if not only_do_alls:
+            viewer_state = s.to_json()
+            output_file = f"{output_dir}/neuroglancer-{current_collection}-{crop}.json"
+            with open(f"{output_file}", "w") as json_file:
+                json.dump(viewer_state, json_file, indent=4)
+            output_file = output_file.replace("/nrs/cellmap/", "nrs/")
+            neuroglancer_url = f"https://neuroglancer-demo.appspot.com#!https://cellmap-vm1.int.janelia.org/{output_file}"
+            all_info.append((crop, current_collection, neuroglancer_url))
 
     if only_do_alls:
+        # Assuming viewer.state.to_json() returns a dictionary
+        viewer_state = s.to_json()
         first_seg_layer = True
         for layer in viewer_state["layers"]:
             if layer["type"] == "segmentation":
@@ -501,29 +263,48 @@ for current_collection in collections:
                 first_seg_layer = False
 
     # Write the JSON data to a file
-    with open(f"{output_dir}/{current_collection}.json", "w") as json_file:
-        json.dump(viewer_state, json_file, indent=4)
     if only_do_alls:
-        neuroglancer_url = f"https://neuroglancer-demo.appspot.com#!https://cellmap-vm1.int.janelia.org/nrs/ackermand/crop_check_updated_{date}/viewer_states_all/{current_collection}.json"
+        output_file = f"{output_dir}/neuroglancer-{current_collection}.json"
+        with open(f"{output_file}", "w") as json_file:
+            json.dump(viewer_state, json_file, indent=4)
+
+        output_file = output_file.replace("/nrs/cellmap/", "nrs/")
+        neuroglancer_url = f"https://neuroglancer-demo.appspot.com#!https://cellmap-vm1.int.janelia.org/{output_file}"
         all_info.append((current_collection, neuroglancer_url))
-    else:
-        neuroglancer_url = f"https://neuroglancer-demo.appspot.com#!https://cellmap-vm1.int.janelia.org/nrs/ackermand/crop_check_updated_{date}/viewer_states/{current_collection}/{crop}.json"
-        all_info.append((crop, current_collection, neuroglancer_url))
 
 import pandas as pd
 
 if only_do_alls:
+    output_csv_name = f"SegmentationChallengeWithNeuroglancerURLs_{date}_all"
     df = pd.DataFrame(all_info, columns=["collection", "url"])
-    df.to_csv(f"SegmentationChallengeWithNeuroglancerURLs_all_{date}.csv", index=False)
 else:
+    output_csv_name = f"SegmentationChallengeWithNeuroglancerURLs_{date}"
     df = pd.DataFrame(all_info, columns=["crop_number", "collection", "url"])
-    df.to_csv(f"SegmentationChallengeWithNeuroglancerURLs_{date}.csv", index=False)
 
+df.to_csv(f"{output_csv_name}.csv", index=False)
+df["url"] = df["url"].replace(
+    f"https://cellmap-vm1.int.janelia.org/nrs/ackermand/viewer_states/crop_check_updated_{date}/",
+    "s3://",
+    regex=True,
+)
+
+## until i update my paths to match s3
+df["url"] = df["url"].replace(
+    "neuroglancer-j",
+    "neuroglancer/viewer_states/neuroglancer-j",
+    regex=True,
+)
+##
+#
+df.to_csv(f"{output_csv_name}_s3.csv", index=False)
 
 # if len(failed_meshification) > 0:
 failed_df = pd.DataFrame(failed_meshification, columns=["failed_paths"])
 failed_df.to_csv(f"failed_meshification_{date}.csv", index=False)
 print(failed_df)
+# %%
+f"https://cellmap-vm1.int.janelia.org/nrs/ackermand/viewer_states/crop_check_updated_{date}/"
+
 # %%
 raw_ds = open_ds(
     f"/nrs/cellmap/data/{current_collection}/{current_collection}.zarr",
