8f269de022413ae529fbfe440c545716a8e548b6
David Ackerman <ackermand@janelia.hhmi.org>
2025-02-24 14:46:21 -0500

    add more options to scripts for when processing non segmentation challenge crops

--- crop_check/check_connectivity/updated/crop_check_connectivity.py
+++ crop_check/check_connectivity/updated/crop_check_connectivity.py
@@ -1,153 +1,214 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[5]:
-
-
+from image_data_interface import ImageDataInterface
 import pandas as pd
 import os
-import yaml
-
-
-def is_dir_empty_except_hidden(path):
-    return all(f.startswith(".") for f in os.listdir(path))
-
-
-collections = ["jrc_mus-nacc-1", "jrc_zf-cardiac-1", "jrc_zf-cardiac-1"]
-crops = [
-    "crop115",
-    "crop378",
-    "crop380",
-]  # [crop115_c1.zarr  crop115_c2.zarr  crop115_c3.zarr  crop378_c1.zarr  crop378_c2.zarr  crop380_c1.zarr  crop380_c2.zarr", "crop380_c3.zarr""]
-for collection, current_crop in zip(collections, crops):
-    for connectivity in [1, 2, 3]:
-        crop = f"{current_crop}_c{connectivity}"
-        zarr_path = f"/nrs/cellmap/bennettd/tmp/{crop}.zarr"
-        if os.path.isdir(zarr_path):
-            for dataset_name in os.listdir(f"{zarr_path}"):
-                if not os.path.isdir(f"{zarr_path}/{dataset_name}"):
-                    continue
-
-                if is_dir_empty_except_hidden(f"{zarr_path}/{dataset_name}/s0"):
-                    continue
-
-                crop_dir = f"/groups/cellmap/cellmap/ackermand/new_meshes/scripts/single_resolution/crop_check_connectivity/{collection}/{crop}/{dataset_name}"
-                os.makedirs(name=crop_dir, exist_ok=True)
-                os.system(f"cp ./default_meshes/dask-config.yaml {crop_dir}")
-
-                run_config_yaml = {
-                    "input_path": f"{zarr_path}/{dataset_name}/s0",
-                    "output_directory": f"/nrs/cellmap/ackermand/new_meshes/meshes/crop_check_connectivity/{collection}/{crop}/{dataset_name}",
-                    "read_write_roi": ["0:256", "0:256", "0:256"],
-                    "downsample_factor": 0,
-                    "target_reduction": 0.0,
-                    "n_smoothing_iter": 0,
-                    "remove_smallest_components": False,
-                    "do_analysis": False,
-                    "do_legacy_neuroglancer": True,
-                }
-
-                with open(f"{crop_dir}/run-config.yaml", "w") as f:
-                    yaml.dump(run_config_yaml, f)
-
-
-# In[22]:
-
-
-import pandas as pd
+import numpy as np
+import logging
+import skimage
+import dask.dataframe as dd
+from dask.distributed import Client, LocalCluster
+import socket
+import os
 import neuroglancer
+import glob
+import json
 import os
 from funlib.persistence import open_ds
-import json
+import click
+
+instance_classes = [
+    "nuc",
+    "vim",
+    "ves",
+    "endo",
+    "lyso",
+    "ld",
+    "perox",
+    "mito",
+    "np",
+    "mt",
+    "cell",
+]
 
 
 def is_dir_empty_except_hidden(path):
     return all(f.startswith(".") for f in os.listdir(path))
 
 
-neuroglancer.set_server_bind_address("0.0.0.0")
+def get_neuroglancer_url(row, input_zarr_path):
+    collection = row["collection"]
+    raw_zarr_path = f"/nrs/cellmap/data/{collection}/{collection}.zarr"
+    output_dir = f"/nrs/cellmap/ackermand/crop_check_updated/viewer_states/{collection}"
+    os.makedirs(output_dir, exist_ok=True)
 
-
-all_info = []
-collections = ["jrc_mus-nacc-1", "jrc_zf-cardiac-1", "jrc_zf-cardiac-1"]
-crops = [
-    "crop115",
-    "crop378",
-    "crop380",
-]  # [crop115_c1.zarr  crop115_c2.zarr  crop115_c3.zarr  crop378_c1.zarr  crop378_c2.zarr  crop380_c1.zarr  crop380_c2.zarr", "crop380_c3.zarr""]
-for collection, current_crop in zip(collections, crops):
-    for connectivity in [1, 2, 3]:
-        crop = f"{current_crop}_c{connectivity}"
-        output_dir = (
-            f"/nrs/cellmap/ackermand/crop_check_connectivity/viewer_states/{collection}"
+    viewer = neuroglancer.Viewer()
+    have_set_position = False
+    with viewer.txn() as s:
+        s.layers["raw"] = neuroglancer.ImageLayer(
+            source=f"zarr://https://cellmap-vm1.int.janelia.org/nrs/data/{collection}/{collection}.zarr/recon-1/em/fibsem-uint8",
         )
-        os.makedirs(output_dir, exist_ok=True)
-        zarr_path = f"/nrs/cellmap/bennettd/tmp/{crop}.zarr"
-        viewer = neuroglancer.Viewer()
-        have_set_position = False
-        with viewer.txn() as s:
-            s.layers["raw"] = neuroglancer.ImageLayer(
-                source=f"zarr://https://cellmap-vm1.int.janelia.org/nrs/data/{collection}/{collection}.zarr/recon-1/em/fibsem-uint8",
-            )
-            if os.path.isdir(zarr_path):
-                for dataset_name in os.listdir(f"{zarr_path}"):
-                    if not os.path.isdir(f"{zarr_path}/{dataset_name}"):
-                        continue
+        crop = f"crop{row.crop_number}"
+        # crop_path = f"/nrs/cellmap/zubovy/crop_splits/combined/{collection}/groundtruth.zarr/{crop}"
+        crop_path = f"{input_zarr_path}/{collection}/groundtruth.zarr/{crop}"
 
-                    if is_dir_empty_except_hidden(f"{zarr_path}/{dataset_name}/s0"):
-                        continue
+        for organelle_path in glob.glob(f"{crop_path}/*"):
+            organelle_name = os.path.basename(organelle_path)
+            if is_dir_empty_except_hidden(f"{organelle_path}/s0"):
+                continue
 
-                    segments = [
-                        int(mesh_id.split(":0")[0])
-                        for mesh_id in os.listdir(
-                            f"/nrs/cellmap/ackermand/new_meshes/meshes/crop_check_connectivity/{collection}/{crop}/{dataset_name}/meshes"
-                        )
-                        if ":0" in mesh_id
-                    ]
-                    image_source = f"zarr://https://cellmap-vm1.int.janelia.org/nrs/bennettd/tmp/{crop}.zarr/{dataset_name}"
-                    mesh_source = f"precomputed://https://cellmap-vm1.int.janelia.org/nrs/ackermand/new_meshes/meshes/crop_check_connectivity/{collection}/{crop}/{dataset_name}/meshes"
-                    s.layers[f"{crop}_{dataset_name}"] = neuroglancer.SegmentationLayer(
-                        source=[image_source, mesh_source], segments=segments
+            if not (organelle_name in instance_classes or organelle_name == "all"):
+                continue
+
+            try:
+                segments = [
+                    int(mesh_id.split(":0")[0])
+                    for mesh_id in os.listdir(
+                        f"/nrs/cellmap/ackermand/new_meshes/meshes/crop_check_updated/{collection}/{crop}/{organelle_name}/meshes"
                     )
+                    if ":0" in mesh_id
+                ]
+            except:
+                print(f"Error in {collection}/{crop}/{organelle_name}")
+                continue
+            image_source = f'zarr://https://cellmap-vm1.int.janelia.org/{crop_path.replace("/nrs/cellmap/","/nrs/")}/{organelle_name}'
+            mesh_source = f"precomputed://https://cellmap-vm1.int.janelia.org/nrs/ackermand/new_meshes/meshes/crop_check_updated/{collection}/{crop}/{organelle_name}/meshes"
+            s.layers[f"{crop}_{organelle_name}"] = neuroglancer.SegmentationLayer(
+                source=[image_source, mesh_source], segments=segments
+            )
 
-                    if dataset_name != "all":
-                        s.layers[f"{crop}_{dataset_name}"].visible = False
+            if organelle_name != "all":
+                s.layers[f"{crop}_{organelle_name}"].visible = False
 
-                    if not have_set_position:
-                        ds = open_ds(
-                            f"/nrs//cellmap/bennettd/tmp/{crop}.zarr",
-                            f"{dataset_name}/s0",
-                        )
-                        position = (ds.roi.begin + ds.roi.end) / 2
-                        s.position = position[::-1]
-                        s.dimensions = neuroglancer.CoordinateSpace(
-                            names=["x", "y", "z"],
-                            units=["nm", "nm", "nm"],
-                            scales=[1, 1, 1],
-                            coordinate_arrays=[
-                                None,
-                                None,
-                                None,
-                            ],
-                        )
+            if not have_set_position:
+                ds = open_ds(
+                    raw_zarr_path,
+                    f"recon-1/labels/groundtruth/{crop}/{organelle_name}/s0",
+                )
+                position = (ds.roi.begin + ds.roi.end) / 2
+                s.position = position[::-1]
+                s.dimensions = neuroglancer.CoordinateSpace(
+                    names=["x", "y", "z"],
+                    units=["nm", "nm", "nm"],
+                    scales=[1, 1, 1],
+                    coordinate_arrays=[
+                        None,
+                        None,
+                        None,
+                    ],
+                )
 
-                        have_set_position = True
+                have_set_position = True
 
-        # Assuming viewer.state.to_json() returns a dictionary
-        viewer_state = viewer.state.to_json()
-        # Write the JSON data to a file
-        with open(f"{output_dir}/{crop}.json", "w") as json_file:
-            json.dump(viewer_state, json_file, indent=4)
-        neuroglancer_url = f"https://neuroglancer-demo.appspot.com#!https://cellmap-vm1.int.janelia.org/nrs/ackermand/crop_check_connectivity/viewer_states/{collection}/{crop}.json"
-        all_info.append((crop, collection, neuroglancer_url))
-
-df = pd.DataFrame(all_info, columns=["crop_number", "collection", "url"])
-
-df.to_csv("SegmentationChallengeCheckConnectivityWithNeuroglancerURLs.csv", index=False)
+    # Assuming viewer.state.to_json() returns a dictionary
+    viewer_state = viewer.state.to_json()
+    # Write the JSON data to a file
+    with open(f"{output_dir}/{crop}.json", "w") as json_file:
+        json.dump(viewer_state, json_file, indent=4)
+    neuroglancer_url = f"https://neuroglancer-demo.appspot.com#!https://cellmap-vm1.int.janelia.org/nrs/ackermand/crop_check_updated/viewer_states/{collection}/{crop}.json"
+    return neuroglancer_url
 
 
-# In[ ]:
+def process_rows(rows, connectivity):
+    logging.getLogger().setLevel(logging.WARNING)
+    # create empty dataframe that matches the input
+    output = []
+    try:
+        for _, row in rows.iterrows():
+            collection = row["collection"]
+            crop_number = row["crop_number"]
+            crop = f"crop{crop_number}"
+            organelle_name = row["organelle_name"]
+            url = row["url"]
+            if organelle_name not in instance_classes:
+                continue
+
+            try:
+                idi = ImageDataInterface(
+                    f"/nrs/cellmap/zubovy/crop_splits/combined/{collection}/groundtruth.zarr/crop{crop_number}/{organelle_name}/s0"
+                )
+                data = idi.to_ndarray_ts()
+                del idi
+            except:
+                raise Exception(
+                    f"/nrs/cellmap/zubovy/crop_splits/combined/{collection}/groundtruth.zarr/crop{crop_number}/{organelle_name}/s0"
+                )
+            reconnected = skimage.measure.label(data > 0, connectivity=connectivity)
+            ids, counts = np.unique(reconnected[reconnected > 0], return_counts=True)
+            ids = ids[counts <= 5]
+            if len(ids) > 100:
+                small_ids = "more than 100"
+            else:
+                small_ids = []
+                for id in ids:
+                    original_id = np.unique(data[reconnected == id])[0]
+                    small_ids.append(original_id)
+            del reconnected, data
+            if len(small_ids) > 0:
+                output.append(
+                    [
+                        collection,
+                        crop,
+                        organelle_name,
+                        url,
+                        small_ids,
+                    ]
+                )
+        return pd.DataFrame(output, columns=rows.columns)
+    except Exception as e:
+        raise Exception(f"Error in {collection} {crop} {organelle_name}: {e}")
 
 
+# get local ip
+@click.command()
+@click.option("--input_zarr_path", "-z", type=str, prompt="Path to crop zarrs")
+@click.option("--input_csv", "-i", type=str, prompt="Path to csv")
+@click.option("--output_dir", "-o", type=str, prompt="Path to output directory")
+@click.option("--connectivity", "-c", type=int, help="Your age.")
+def main(input_zarr_path, input_csv, output_dir, connectivity):
+    os.makedirs(output_dir, exist_ok=True)
+    original_df = pd.read_csv(input_csv)
+    new_df_data = []
+    for _, row in original_df.iterrows():
+        url = get_neuroglancer_url(row, input_zarr_path)
+        crop_number = row["crop_number"]
+        collection = row["collection"]
+        # crop_path = f"/nrs/cellmap/zubovy/crop_splits/combined/{collection}/groundtruth.zarr/crop{crop_number}"
+        crop_path = f"{input_zarr_path}/{collection}/groundtruth.zarr/crop{crop_number}"
+        for organelle_path in glob.glob(f"{crop_path}/*"):
+            organelle_name = os.path.basename(organelle_path)
+            if organelle_name in instance_classes:
+                new_df_data.append([collection, crop_number, organelle_name, url, []])
+
+    new_df = pd.DataFrame(
+        new_df_data,
+        columns=[
+            "collection",
+            "crop_number",
+            "organelle_name",
+            "url",
+            "small_object_ids",
+        ],
+    )
+    # new_df.to_csv("testing.csv")
+    cluster = LocalCluster(
+        n_workers=int(os.cpu_count() / 8), threads_per_worker=1, memory_limit="64GB"
+    )
+    with Client(cluster) as client:
+        ddf = dd.from_pandas(
+            new_df,
+            npartitions=100,
+        )
+        print(
+            f'Check {client.cluster.dashboard_link.replace("127.0.0.1",socket.gethostname())}'
+        )
+        result_ddf = ddf.map_partitions(
+            process_rows, connectivity=connectivity, meta=ddf
+        )
+        output_df = result_ddf.compute()
+
+        output_df.to_csv(
+            f"{output_dir}/isolated_pixels_connectivity_{connectivity}.csv", index=False
+        )
 
 
+if __name__ == "__main__":
+    main()
